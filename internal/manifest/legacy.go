f2fc1842a334d5a6a7701f748bdd25dbc6a53e5d
Antonio Mariani <antonio.mariani@cmcc.it>
2026-09-18 17:26:50 +0200

    add ReadLegay function and add legacy_test.go

--- internal/manifest/legacy.go
+++ internal/manifest/legacy.go
@@ -1,9 +1,11 @@
 package manifest
 
 import (
+	"bufio"
 	"fmt"
 	"io"
 	"sort"
+	"strings"
 )
 
 func WriteLegacy(w io.Writer, m Manifest) error {
@@ -24,4 +26,43 @@ func WriteLegacy(w io.Writer, m Manifest) error {
 	}
 
 	return nil
+}
+
+func ReadLegacy(r io.Reader) (Manifest, error) {
+	m := Manifest{
+		Entries: make(map[string]Entry),
+	}
+
+	scanner := bufio.NewScanner(r)
+	lineCount := 0
+	for scanner.Scan() {
+		lineCount++
+
+		line := scanner.Text()
+		if line == "" {
+			continue
+		}
+
+		// expected string: MD5 FILENAME
+		sum, name, found := strings.Cut(line, " ")
+		if !found || sum == "" || name == "" {
+			return Manifest{}, fmt.Errorf("invalid manifest entry at line %d", lineCount)
+		}
+
+		// this should never happens
+		if _, exists := m.Entries[name]; exists {
+			return Manifest{}, fmt.Errorf("duplicate entry for %s at line %d", name, lineCount)
+		}
+
+		m.Entries[name] = Entry{
+			Name: name,
+			MD5:  sum,
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return m, fmt.Errorf("failed to read legacy manifest: %w", err)
+	}
+
+	return m, nil
 }
