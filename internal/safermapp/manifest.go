d1fe81741e18d6da7b891c2bd2f282641ae3329e
Antonio Mariani <antonio.mariani@cmcc.it>
2026-09-25 10:37:52 +0200

    add validation in manifest entry

--- internal/safermapp/manifest.go
+++ internal/safermapp/manifest.go
@@ -43,14 +43,53 @@ func addManifestToIndex(index fileIndex, manifestFile string) error {
 
 	// Manifest Indexing
 	for _, entry := range m.Entries {
-		key := fileKey{
-			name: entry.Name,
-			md5:  entry.MD5,
+		key, record, err := indexRecord(manifestDir, entry)
+		if err != nil {
+			return fmt.Errorf("failed to index entry: %w", err)
 		}
-		index[key] = append(index[key], fileRecord{
-			path:        filepath.Join(manifestDir, entry.Name),
-			manifestDir: manifestDir,
-		})
+		index[key] = append(index[key], record)
 	}
+	return nil
+}
+
+func indexRecord(manifestDir string, entry manifest.Entry) (fileKey, fileRecord, error) {
+
+	err := isValidEntry(entry)
+	if err != nil {
+		return fileKey{}, fileRecord{}, err
+	}
+
+	key := fileKey{
+		name: entry.Name,
+		md5:  entry.MD5,
+	}
+	record := fileRecord{
+		path:        filepath.Join(manifestDir, entry.Name),
+		manifestDir: manifestDir,
+	}
+	return key, record, nil
+}
+
+func isValidEntry(entry manifest.Entry) error {
+	// Check on file name
+	if entry.Name == "" {
+		return fmt.Errorf("empty entry name")
+	}
+	if !filepath.IsLocal(entry.Name) {
+		return fmt.Errorf("not local entry: %s", entry.Name)
+	}
+	if entry.Name == "." || entry.Name == ".." {
+		return fmt.Errorf("invalid entry: %s", entry.Name)
+	}
+
+	// Check on MD5
+	if entry.MD5 == "" {
+		return fmt.Errorf("empty entry MD5")
+	}
+
+	if len(entry.MD5) != 32 {
+		return fmt.Errorf("invalid MD5 length: %d", len(entry.MD5))
+	}
+
 	return nil
 }
